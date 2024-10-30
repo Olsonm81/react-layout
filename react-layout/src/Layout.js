1e17e5db26c6d435faa7b479de9bc590af64103f
Matthew <olsonm811@gmail.com>
2024-10-29 21:19:10 -0400

    adding in class example

--- react-layout/src/Layout.js
+++ react-layout/src/Layout.js
@@ -1,15 +1,24 @@
 import {Outlet, Link} from "react-router-dom";
+import {useState} from "react";
+import "./css/Layout.css";
 import Header from "./components/Header";
 const Layout = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <>
             <Header />
-
+            <button onClick={toggleMenu}>Toggle</button>
             
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/pictures">Pictures</Link>
-
+            <nav id="main-nav" className={menuOpen?"":"hide-small"}>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/pictures">Pictures</Link>
+            </nav>
             <Outlet />
 
             <p>This is my footer</p>
